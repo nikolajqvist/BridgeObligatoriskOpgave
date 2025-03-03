f18e2804fead81e7dba36e2b56838743f3703afe
Nikolaj Qvist <nqvistmoller@hotmail.com>
2025-03-03 11:34:59 +0100

    Add project files.

---
+++ Bridge/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// This is the car class called car.
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// This is the licensplate of a car.
+        /// </summary>
+        public string Licensplate { get; set; }
+        /// <summary>
+        /// This is the date and time the car is driving across the bridge.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// This is the car constructor
+        /// </summary>
+        public Car(string licensplate, DateTime date)
+        {
+            Licensplate = licensplate;
+            Date = date;
+        }
+        public double Price()
+        {
+            return 230.0;
+        }
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ BridgeTest/Test1.cs
@@ -0,0 +1,31 @@
+﻿using Bridge;
+
+namespace BridgeTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod_Price()
+        {
+            Car car1 = new Car("XK72828", DateTime.Now);
+
+            var expectedprice = car1.Price();
+
+            double result = 230;
+
+            Assert.IsTrue(expectedprice == result);
+        }
+        [TestMethod]
+        public void TestMethod_ReturnString()
+        {
+            Car c1 = new Car("FJ46726", DateTime.Now);
+
+            string iscar = "Car";
+
+            var expected = c1.VehicleType();
+
+            Assert.IsTrue(expected == iscar);
+        }
+    }
+}
